8d2ca11056aa194676837f972460a0e1e3bf57cc
Ulexus <ulexus@gmail.com>
2015-01-02 00:24:28 -0500

    Redirect doubled path separator

--- web/server/target.js
+++ web/server/target.js
@@ -1,6 +1,9 @@
 var fs = Npm.require('fs');
 
 Router.map(function() {
+   this.route('/target/:camera//:filename',{
+      this.redirect('/target/'+this.params.camera+'/'+this.params.filename);
+   }),
    this.route('/target/:camera/:filename', function() {
       var req = this.request.body;
       var fn = this.params.filename
